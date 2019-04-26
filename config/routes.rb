d32d3bfa7c8e014ca17930264398bd4dad86161c
hugo escandon <hescandon6@gmail.com>
2019-04-26 13:29:29 -0400

    make it better 4.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 
-  resources :videos
   devise_for :users
+  resources :videos
   resources :weathers
   resources :songs
   resources :playlists
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_131721) do
+ActiveRecord::Schema.define(version: 2019_04_26_172824) do
 
   create_table "moods", force: :cascade do |t|
     t.string "emoji"
