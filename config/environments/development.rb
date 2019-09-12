d66ed61573f8cfe0db867d1d616dff129770d6de
hugo escandon <hescandon6@gmail.com>
2019-09-12 15:34:09 -0400

    logs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 4000 }
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.log_level = :info
   # Do not eager load code on boot.
   config.eager_load = false
 
