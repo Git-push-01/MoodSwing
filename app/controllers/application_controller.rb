e9bc98b116c9ecfa2372684144ea6afa740407e4
hugo escandon <hescandon6@gmail.com>
2019-09-14 16:34:38 -0400

    rubylogs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ end
 def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
+  def append_info_to_payload(payload)
+      super
+     case
+        when payload[:status] == 200
+          payload[:level] = "INFO"
+        when payload[:status] == 302
+          payload[:level] = "WARN"
+        else
+          payload[:level] = "ERROR"
+        end
+end
 
 
 end
