c6cc6abe8af483a68bc3f816785de6d70fdcd557
hugo escandon <hescandon6@gmail.com>
2019-09-16 13:33:26 -0400

    datadog -statsd5.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,17 +8,7 @@ end
 def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
-  def append_info_to_payload(payload)
-      super
-     case
-        when payload[:status] == 200
-          payload[:level] = "INFO"
-        when payload[:status] == 302
-          payload[:level] = "WARN"
-        else
-          payload[:level] = "ERROR"
-        end
-end
-# for the purpose of pushing code to see if datadog monitor is working
-# test 2 for datadog agent webhook
+
+
+
 end
