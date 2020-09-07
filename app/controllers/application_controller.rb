5e58e49293066f224e7e1091a0147b7cd95470dc
Hugo Escandon <32473254+Git-push-01@users.noreply.github.com>
2020-09-07 11:34:23 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,10 @@ end
 def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
-
+def iframe_action
+      response.headers.delete "X-Frame-Options"
+      render_something
+    end
 
 
 end
